ea5820cf048ef2d1eb696f9feb6ef512934dfc34
Francis Viviers <viviersf@gmail.com>
2022-05-31 09:36:21 +0200

    Make demo-crc.sh more modular and intelligent

--- demo-crc.sh
+++ demo-crc.sh
@@ -1,48 +1,86 @@
 #!/bin/bash
 
-# Enable / Disable Telemetry to Red Hat
-crc config set consent-telemetry no
+setCRCConfigs (){
+    # Enable / Disable Telemetry to Red Hat
+    crc config set consent-telemetry no
 
-# Enable / Disable Cluster Monitoring 
-# Enable only if you have enough memory, needs ~4G extra
-crc config set enable-cluster-monitoring false
+    # Enable / Disable Cluster Monitoring 
+    # Enable only if you have enough memory, needs ~4G extra
+    crc config set enable-cluster-monitoring false
 
-# Set CRC CPU's
-crc config set cpus 16
+    # Set CRC CPU's
+    crc config set cpus 16
 
-# Set CRC Memory
-crc config set memory 24576
+    # Set CRC Memory
+    crc config set memory 24576
 
-# Set Disk Size, Thin Provisioned
-crc config set disk-size 250
+    # Set Disk Size, Thin Provisioned
+    crc config set disk-size 250
 
-# Set Pull secret
-#crc config set pull-secret-file ~/.crc/pull-secret.txt
+    # Set Pull secret
+    #crc config set pull-secret-file ~/.crc/pull-secret.txt
 
-# Show config
-crc config view
+    # Show config
+    crc config view
 
-# Set up CRC if needed
-crc setup
+    # Set up CRC if needed
+    crc setup
+}
 
-# Start CRC
-crc start
+createCRCPersistantStorage(){
+    # Attach secondary disk for persistant storage - Thin provisioned
+    # Create Disk
+    sudo -S qemu-img create -f raw ~/.crc/vdb 100G
+    # Attach Disk
+    sudo virsh attach-disk crc ~/.crc/vdb vdb --cache none
+}
 
-# Attach secondary disk for persistant storage - Thin provisioned
-# Create Disk
-sudo -S qemu-img create -f raw ~/.crc/vdb 100G
-# Attach Disk
-sudo virsh attach-disk crc ~/.crc/vdb vdb --cache none
+crcLogin(){
+    # Loging to CRC
+    echo "Logging in:"
+    $(crc console --credentials | awk -F"'" '$0=$2' | grep kubeadmin)
+}
 
-# Command to SSH to VM If needed
-export alias crcssh='ssh -p 22 -i ~/.crc/machines/crc/id_ecdsa core@"$(crc ip)"'
+setupArgoCDPermissions(){
+    # Give ARGOCD cluster admin to CRC for demo purposes
+    oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:openshift-gitops:openshift-gitops-argocd-application-controller
+}
 
-# SSH Into CRC and Show uptime
-crcssh uptime
+startupOptions(){
+    echo "No startup options specified. Valid options:"
+    echo "============================================"
+    echo "* Set Up demo CRC: $0 setup"
+    echo "* Log into CRC with kubeadmin: $0 login"
+    echo -e "\n"
+    exit 1
+}
 
-# Loging to CRC
-alias crclogin=$(crc console --credentials | awk -F"'" '$0=$2' | grep kubeadmin)
-crclogin
+setupCRC(){
 
-# Give ARGOCD cluster admin to CRC for demo purposes
-oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:openshift-gitops:openshift-gitops-argocd-application-controller
+    # Set CRC Configs
+    setCRCConfigs
+
+    # Create & Attach Storage
+    createCRCPersistantStorage
+
+    # Start CRC
+    crc start
+
+    # Command to SSH to VM If needed
+    alias crcssh='ssh -p 22 -i ~/.crc/machines/crc/id_ecdsa core@"$(crc ip)"'
+
+    # SSH Into CRC and Show uptime
+    crcssh uptime
+
+    # Log Into CRC
+    crcLogin
+
+}
+
+# Startup
+case "${1}" in
+    setup)      setupCRC ;;
+    login)      crcLogin ;;
+    "")         startupOptions ;;
+    *)          startupOptions ;;
+esac
